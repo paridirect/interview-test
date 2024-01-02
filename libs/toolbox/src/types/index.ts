7a9931cdc0dd26917e05b9df329730c77360e12b
Danny Franca <me@dannyfranca.com>
2024-01-02 21:24:51 +0000

    feat(toolbox): add ErrorCause enum

--- libs/toolbox/src/types/index.ts
+++ libs/toolbox/src/types/index.ts
@@ -1,10 +1,19 @@
 export type Primitive = string | number | boolean | bigint | symbol | undefined | null;
 
+export interface UseCase<I = unknown, O = unknown> {
+  execute(input: I): Promise<O>;
+}
+
 export enum GetManyOrder {
   ASC = 'asc',
   DESC = 'desc',
 }
 
-export interface UseCase<I = unknown, O = unknown> {
-  execute(input: I): Promise<O>;
+export enum ErrorCause {
+  BAD_INPUT = 'bad_input',
+  NOT_FOUND = 'not_found',
+  UNAUTHORIZED = 'unauthorized',
+  FORBIDDEN = 'forbidden',
+  CONFLICT = 'conflict',
+  INTERNAL = 'internal',
 }
