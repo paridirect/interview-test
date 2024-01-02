987be92f831a72e40fc3acf025d03061facd005a
Danny Franca <me@dannyfranca.com>
2024-01-02 21:52:25 +0000

    refactor(toolbox): remove uneeded id util

--- libs/toolbox/src/utils/id.spec.ts
+++ libs/toolbox/src/utils/id.spec.ts
@@ -1,17 +1,4 @@
-import { createSortableUid, createRandomUid } from './id';
-
-describe(createSortableUid.name, () => {
-  it('should return a string', () => {
-    const uid = createSortableUid();
-    expect(typeof uid).toBe('string');
-  });
-
-  it('should return a UUID v7 string', () => {
-    const uid = createSortableUid();
-    const uuidV7Regex = /^[0-9a-f]{8}-[0-9a-f]{4}-7[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-    expect(uuidV7Regex.test(uid)).toBe(true);
-  });
-});
+import { createRandomUid } from './id';
 
 describe(createRandomUid.name, () => {
   it('should return a string', () => {
--- libs/toolbox/src/utils/id.ts
+++ libs/toolbox/src/utils/id.ts
@@ -1,13 +1,6 @@
-import { uuidv7 } from 'uuidv7';
 import { randomUUID } from 'uncrypto';
 
 export type UUID = `${string}-${string}-${string}-${string}-${string}`;
-
-/**
- * Creates a new sortable UUID that can be sorted by date.
- * @returns A new sortable UUID
- */
-export const createSortableUid = (): UUID => uuidv7() as UUID;
 
 /**
  * Creates a new random Unique UUID.
