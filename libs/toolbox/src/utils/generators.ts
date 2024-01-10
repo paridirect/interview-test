793db505f0b924701bfff58c826eaa9f8617b166
Danny Franca <me@dannyfranca.com>
2024-01-10 16:18:22 +0000

    feat: add oneOf helper

--- libs/toolbox/src/utils/generators.ts
+++ libs/toolbox/src/utils/generators.ts
@@ -4,3 +4,4 @@ export const randomWord = () => faker.lorem.word();
 export const randomSentence = () => faker.lorem.sentence();
 export const randomInt = (opt?: { min?: number; max?: number }) => faker.number.int(opt);
 export const randomDate = (opt: { from: Date; to: Date }) => faker.date.between(opt);
+export const oneOf = <T>(arr: T[]) => arr[randomInt({ min: 0, max: arr.length - 1 })];
