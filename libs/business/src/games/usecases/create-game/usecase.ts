687339c3ea4857463ca11174ae1778db50346a94
Danny Franca <me@dannyfranca.com>
2024-01-10 16:10:46 +0000

    feat: add validation for categories and description

--- libs/business/src/games/usecases/create-game/usecase.ts
+++ libs/business/src/games/usecases/create-game/usecase.ts
@@ -2,7 +2,7 @@ import { type UseCase, createRandomUid, ErrorCause } from '@paridirect/toolbox';
 
 import type { CreateGameUseCaseInput, CreateGameUseCaseOutput } from './types';
 import { type GameRepository } from '../../repositories';
-import { type GameProps } from '../../entities';
+import { type GameProps, existingCategories } from '../../entities';
 
 export class CreateGameUseCase implements UseCase {
   constructor(private readonly repo: GameRepository) {}
@@ -10,6 +10,12 @@ export class CreateGameUseCase implements UseCase {
   async execute(input: CreateGameUseCaseInput): Promise<CreateGameUseCaseOutput> {
     if (input.name.length < 3 || input.name.length > 25)
       throw new Error('Game name must be between 3 and 25 characters', { cause: ErrorCause.BAD_INPUT });
+
+    if (input.description.length < 10 || input.name.length > 150)
+      throw new Error('Game name must be between 10 and 150 characters', { cause: ErrorCause.BAD_INPUT });
+
+    if (!existingCategories.has(input.category))
+      throw new Error('Game category does not exist', { cause: ErrorCause.NOT_FOUND });
 
     const transactionAgreement: GameProps = {
       ...input,
